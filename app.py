2889170f9681c778644ccba95e16fb93b3f88886
Colombao <joaoocolombo@gmail.com>
2024-05-08 20:45:08 -0300

    front-end novo

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
-import re
-from datetime import datetime
-
-from flask import Flask
+import sqlite3
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Função para criar as tabelas no banco de dados
+def create_tables():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS vector_info (
+                        id INTEGER PRIMARY KEY,
+                        name TEXT NOT NULL,
+                        description TEXT NOT NULL
+                    )''')
 
+    cursor.execute('''CREATE TABLE IF NOT EXISTS random_order (
+                        id INTEGER PRIMARY KEY,
+                        vector_id INTEGER,
+                        order_index INTEGER,
+                        value INTEGER,
+                        FOREIGN KEY (vector_id) REFERENCES vector_info(id)
+                    )''')
+    conn.commit()
+    conn.close()
+
+# Executar a função para criar as tabelas
+create_tables()
+
+# Rotas Flask...
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/home")
 def home():
-    return "Hello, Flask!"
+    return render_template("home.html")
 
-
-@app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+if __name__ == "__main__":
+    app.run(debug=True)
